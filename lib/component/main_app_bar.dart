0476bd367d6d2d25d36679daa82ef28246c20109
dlaehdrlf4 <dlaehdrlf4@naver.com>
2023-02-24 00:59:48 +0900

    first app

---
+++ lib/component/main_app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../const/colors.dart';
+
+class MainAppBar extends StatelessWidget {
+  const MainAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+      final ts = TextStyle(fontSize: 30.0,color: Colors.white);
+
+    return SliverAppBar(
+      expandedHeight: 500,
+      backgroundColor: primaryColor,
+      flexibleSpace: FlexibleSpaceBar(
+        background: SafeArea(
+          child: Column(
+            children: [
+              Text('서울',style: ts,),
+              Text('${DateTime.now()}',style: ts.copyWith(fontSize: 20.0),),
+              SizedBox(height: 20.0,),
+              Image.asset('asset/img/mediocre.png',width: MediaQuery.of(context).size.width /2,),
+              SizedBox(height: 20.0,),
+              Text('보통',style: ts.copyWith(fontWeight: FontWeight.w700,fontSize: 40.0),),
+              SizedBox(height: 10.0,),
+              Text('나쁘지 않네요',style: ts.copyWith(fontWeight: FontWeight.w700,fontSize: 20.0),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:dusty_dust/const/colors.dart';
+import 'package:flutter/material.dart';
+
+import '../component/main_app_bar.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: CustomScrollView(
+        slivers: [
+          MainAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
